cad779c767427fce4ddd66b819200f0b359de91b
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-04 19:14:25 +0100

    Changed age in user.js (vercel auto deploy on pull request test)

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     try {
-        res.status(200).json({ name: 'Jakub', surName: 'Kaminski', age: 25, department: 'IT' });
+        res.status(200).json({ name: 'Jakub', surName: 'Kaminski', age: 26, department: 'IT' });
     } catch (err) {
         res.status(500).send({ error: 'failed to fetch data' });
     }
