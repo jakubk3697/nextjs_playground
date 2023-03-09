bc116122400516510124bd283d77f1a31f428cff
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-09 14:38:02 +0100

    Prepared static props fetching structure

--- NextJS-tailwind/movie-app/pages/index.tsx
+++ NextJS-tailwind/movie-app/pages/index.tsx
@@ -1,9 +1,26 @@
 import { Hero } from "@/components/Hero";
 
-export default function Home() {
+type moviesProps = {
+  movies: {
+    name: string;
+  }
+}
+
+export default function Home({movies}: moviesProps) {
+  console.log(movies);
   return (
     <>
       <Hero/>
     </>
   )
 }
+
+export async function getStaticProps() {
+  const movies = [{name: 'hello'}];
+
+  return {
+    props: {movies},
+  }
+}
+
+// https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
