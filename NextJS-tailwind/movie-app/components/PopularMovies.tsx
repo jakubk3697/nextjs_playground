171f40da94d95019d332056aaf3dd1b0d1a58328
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-09 19:43:32 +0100

    Updated current components and next.config.js

--- NextJS-tailwind/movie-app/components/PopularMovies.tsx
+++ NextJS-tailwind/movie-app/components/PopularMovies.tsx
@@ -1,21 +1,17 @@
+import { MovieCard } from "./MovieCard";
+
 type Movies = {
     movies: object[];
 }
 
 export const PopularMovies = ({ movies }: Movies) => {
-    console.log({ movies });
 
     return (
-        <div className="bg-gray-700 container max-w-7xl mx-auto pb-10 px-4">
+        <div className="bg-gray-700 container max-w-7xl mx-auto pb-10 px-4 flex-wrap">
             <h1 className="text-white text-2xl mt-8 mb-5">Popular</h1>
-            {movies.map((movie: any) => {
-                return (
-                    <div key={movie.id} className="">
-                        <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title} className="w-1/2" />
-                        <h1 className="text-white text-lg">{movie.title}</h1>
-                    </div>
-                )
-            })}
+            <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+                {movies.map((movie: any) => <MovieCard movie={movie} key={movie.id} />)}
+            </div>
         </div>
     )
 }
--- NextJS-tailwind/movie-app/pages/contact.tsx
+++ NextJS-tailwind/movie-app/pages/contact.tsx
@@ -1,9 +1,11 @@
+import Meta from '@/components/Meta'
 import Image from 'next/image'
 
 export default function Contact() {
     return (
         <div className="text-center bg-white pb-10">
-            <h1 className="text-2xl text-gray-700 uppercase font-bold">Contact us</h1>
+            <Meta title="Contact Us"/>
+            <h1 className="text-2xl text-gray-700 uppercase font-bold">Contact Us</h1>
             <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur.</p>
             <Image
                 src={"/contact_us.png"}
