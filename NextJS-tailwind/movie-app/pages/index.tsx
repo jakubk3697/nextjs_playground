394d826f688751d790a8dd0083165f13b662c30c
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-09 14:53:09 +0100

    Added axios

--- NextJS-tailwind/movie-app/pages/index.tsx
+++ NextJS-tailwind/movie-app/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Hero } from "@/components/Hero";
+import { server } from "@/config";
 import axios from "axios";
 
 type moviesProps = {
@@ -7,17 +8,17 @@ type moviesProps = {
   }
 }
 
-export default function Home({movies}: moviesProps) {
+export default function Home({ movies }: moviesProps) {
   console.log(movies);
   return (
     <>
-      <Hero/>
+      <Hero />
     </>
   )
 }
 
 export async function getStaticProps() {
-  const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`);
+  const res = await axios.get(`${server}movie/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`);
 
   return {
     props: {
@@ -25,5 +26,3 @@ export async function getStaticProps() {
     }
   }
 }
-
-// https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
