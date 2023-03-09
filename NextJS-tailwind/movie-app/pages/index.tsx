3a664fdbc5136b5a0d2d3c3375289c0dd3f02673
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-09 13:57:20 +0100

    Created initial component using Tailwind and TS

--- NextJS-tailwind/movie-app/pages/index.tsx
+++ NextJS-tailwind/movie-app/pages/index.tsx
@@ -1,14 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] });
+import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <Hero/>
     </>
   )
 }
